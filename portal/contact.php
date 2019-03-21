7281af581b581385899d57b02863f7ce116f6613
‘king’ <2962938812@qq.com>
2019-03-21 10:22:26 +0800

    ‘week_jiaoyu

---
+++ portal/contact.php
@@ -0,0 +1,13 @@
+<?php echo '周末设计商业模板保护！请购买正版模板, 客服QQ: 441576729 , http://www.haoweek.com/ ';exit;?>
+<div class="wk_c_left_cont">
+    <span class="wk_c_left_cont1">$week_lang[86]</span><span class="wk_c_left_cont2">$week_lang[87]</span>
+</div>
+<div class="wk_left_contdiv"> 
+    <span>$week_lang[88]</span>
+    <span>$week_lang[89]</span>
+    <span>$week_lang[90]</span>
+    <span>$week_lang[91]</span>
+    <span>$week_lang[92]</span>
+    <span>$week_lang[93]</span>
+</div>
+<div class="clear"></div>
